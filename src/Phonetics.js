0683f9a9d7fbb2f2832a665596b63405779ea789
Ira Filonova <74858792+wonder-filka@users.noreply.github.com>
2022-09-19 19:47:16 +0300

    Update Phonetics.js

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,17 +2,15 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  if (props.phonetics.audio != "" && props.phonetics.text != "") {
-    return (
-      <div className="Phonetic">
-        <figure className="p-0 m-0">
-          <audio controls src={props.phonetics.audio}>
-            Your browser does not support the
-            <code>audio</code> element.
-          </audio>
-        </figure>
-        {props.phonetics.text}
-      </div>
-    );
-  }
+  return (
+    <div className="Phonetic">
+      <figure className="p-0 m-0">
+        <audio controls src={props.phonetics.audio}>
+          Your browser does not support the
+          <code>audio</code> element.
+        </audio>
+      </figure>
+      {props.phonetics.text}
+    </div>
+  );
 }
